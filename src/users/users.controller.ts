b0210cb7ec3db1814440e85231b0fe211f171c44
Nahom <nahomdebele002@gmail.com>
2023-11-16 22:22:18 +0300

    typeorm integration done and user auth done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUseerDto } from './dtos/create-users.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    @Post('/signup')
+    createUser(@Body() boyd:CreateUseerDto) {
+        console.log(boyd);
+    }
+    
+}
